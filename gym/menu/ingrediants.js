cecb1073eac4b7783d6d47e861c64135666f0e84
Othmane kun <156709799+Othmanekhomri@users.noreply.github.com>
2026-09-15 20:45:29 +0100

    Add files via upload

---
+++ gym/menu/ingrediants.js
@@ -0,0 +1,16 @@
+/* =========================
+   MOBILE NAV
+========================= */
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
